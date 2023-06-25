ce4172b38243ab153675faed967604c1f29f4ed6
LeylaKodalci <54359880+lucp9827@users.noreply.github.com>
2023-06-25 17:05:23 +0200

    Update Yuen.R

--- Methods/Yuen.R
+++ Methods/Yuen.R
@@ -7,7 +7,6 @@
 # REFERENCES
 # Yuen, K. K. (1974). The two sample trimmed t for unequal population variances. Biometrika, 61, 165-170 (https://cran.r-project.org/web/packages/WRS2/vignettes/WRS2.pdf)
 # END
-
 Test<-function(db) {
   results<-WRS2::yuen(db$y~db$group)
   return(list(
